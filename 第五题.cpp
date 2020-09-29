500a7fdeadd07d40de10b4b4dbf7635eed448156
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-09-29 19:21:08 +0800

    Add files via upload

--- 第五题.cpp
+++ 第五题.cpp
@@ -7,11 +7,12 @@ int main()
     int x=0;
     int y=0;
     int time=0;
-    int i=-1;
-    scanf("%s",&zifu);
+    int i=0;
+    scanf("%[^\n]",&zifu);
     time=strlen(zifu);
     for(y=0;y<time;y++)
     {
+    	i=zifu[y];
     	if(zifu[y]==',')
     	{
     		for(x=y;x<time;x++)
@@ -19,6 +20,7 @@ int main()
     			zifu[x]=zifu[x+1];
 			}
 			time--;
+			y--;
 		}
 		else if(zifu[y]=='.')
     	{
@@ -27,19 +29,57 @@ int main()
     			zifu[x]=zifu[x+1];
 			}
 			time--;
+			y--;
 		}
-		else if(zifu[y]=='\0')
+		else if(zifu[y]=='£¬')
     	{
     		for(x=y;x<time;x++)
     		{
     			zifu[x]=zifu[x+1];
 			}
 			time--;
+			y--; 
+		}
+		else if(zifu[y]==':')
+    	{
+    		for(x=y;x<time;x++)
+    		{
+    			zifu[x]=zifu[x+1];
+			}
+			time--;
+			y--;
+		}
+		else if(i==32)
+    	{
+    		for(x=y;x<time;x++)
+    		{
+    			zifu[x]=zifu[x+1];
+			}
+			time--;
+			y--;
+		}
+		else if(i>64&&i<91)
+    	{
+    		zifu[y]=zifu[y]+32; 
 		}
 	}
-	for(y=0;y<time;y++)
+	time=strlen(zifu);
+	i=1;
+	x=time/2;
+	for(y=0;y<=x;y++)
 	{
-		printf("%c\n",zifu[y]);
+		if(zifu[y]!=zifu[time-y-1])
+		{
+			i=0;
+		}
+	}
+	if(i==1)
+	{
+		printf("ture");
+	}
+	else
+	{
+		printf("false");
 	}
 	return 0;
 }
