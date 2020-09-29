cdc72c6639c8c1fa1a7f525663c54eee0aaf38ea
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-09-29 13:20:30 +0800

    Add files via upload

---
+++ 第四题.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+    int num[100];
+    char zifu[100];
+    int x=0;
+    int y=0;
+    int time=0;
+    int i=0;
+    zifu[0]='1';
+    scanf("%s",&zifu);
+    time=strlen(zifu)-1;
+    for(y=0;y<=time;y++)
+    {
+    	if(zifu[y]==',')
+    	{
+    		for(x=y;x<time;x++)
+    		{
+    			zifu[x]=zifu[x+1];
+			}
+			time--;
+		}
+		num[y]=zifu[y]-'0';
+	}
+	for(y=0;y<=time;y++)
+	{
+		if(y==num[y])
+		{
+			i=y;
+			break;
+		}
+	}
+	printf("%d",i);
+	return 0;
+}
