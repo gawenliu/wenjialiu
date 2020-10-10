1a4ce3548c2f112409e47273f9e261508f462c84
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-10-10 22:05:43 +0800

    Update 第二题.cpp

--- 第二题.cpp
+++ 第二题.cpp
@@ -4,16 +4,13 @@ int main()
 {
     int score[100];
     char zifu[100];
-    int x=0;
-    int y=0;
-    int time=0;
-    int sum=0;
+    int a,b,x,y,z,time,sum=0;
     zifu[0]='1';
     scanf("%s",&zifu);
-    time=strlen(zifu)-1;
+    time=strlen(zifu)-1; 
     for(y=0;y<=time;y++)
     {
-    	if(zifu[y]==',')
+    	if(zifu[y]=='"')
     	{
     		for(x=y;x<time;x++)
     		{
@@ -22,35 +19,63 @@ int main()
 			time--;
 		}
 	}
+	y=0;
     for(x=0;x<=time;x++)
     {
-        if(zifu[x]=='+')
+    	score[y]=0;
+    	if(zifu[x]=='-')
+    	{
+    		z=x+1;
+    		while(zifu[z]>='0'&&zifu[z]<='9')
+    		{
+    			z++;
+			} 
+			for(a=x+1;a<z;a++)
+			{
+				b=zifu[a]-'0';
+				score[y]=score[y]*10+b;
+			}
+			x=z;
+			score[y]=0-score[y];
+		}
+        else if(zifu[x]=='+')
         {
-            score[x]=score[x-1]+score[x-2];
-            sum=sum+score[x];
+            score[y]=score[y-1]+score[y-2];
         }
         else if(zifu[x]=='D')
         {
-        score[x]=score[x-1]+score[x-1];
-            sum=sum+score[x];
+            score[y]=score[y-1]+score[y-1];
         }
         else if(zifu[x]=='C')
         {
-            sum=sum-score[x-1];
-            y=x-1;
-            for(x=y;x<=time-1;x++)
-            {
-            	zifu[x]=zifu[x+2];
-			}
-			time=time-2;
-			x=y-1;
+        	y--;
+        	score[y]=0;
+        	y--;
         }
-        else
+        else if(zifu[x]>='0'&&zifu[x]<='9')
         {
-            score[x]=zifu[x]-'0';
-            sum=sum+score[x];
-        }
+        	z=x;
+    		while(zifu[z]>='0'&&zifu[z]<='9')
+    		{
+    			z++;
+			}
+			for(a=x;a<z;a++)
+			{
+				b=zifu[a]-'0';
+				score[y]=score[y]*10+b;
+			}
+			x=z;
+		}
+		else if(zifu[x]==','||zifu[x]=='，'||zifu[x]=='[')
+		{
+			y--;
+		 }  
+        y++;
     }
+    for(a=0;a<y;a++)
+    {
+    	sum+=score[a]; 
+	}
     printf("%d",sum);
     return 0;
 }
