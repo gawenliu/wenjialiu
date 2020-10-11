dcfcaa9cc5decb240d7755b02525d2277b84db92
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-10-11 18:55:48 +0800

    Update 第三题.cpp

--- 第三题.cpp
+++ 第三题.cpp
@@ -4,39 +4,28 @@ int main()
 {
 	char x[100];
 	int i[100];
-	int a=0;
-	int b=0;
-	int len=0;
-	int ji=0;
-	int ou=0;
+	int a=0,b=0,len=0,ji=0,ou=0;
 	printf("location=");
 	scanf("%s",&x);
 	len=strlen(x);
 	for(a=0;a<len;a++)
-    {
-    	if(x[a]==',')
-    	{
-    		b=a;
-    		for(a=a;a<len;a++)
-    		{
-    			x[a]=x[a+1];
-			}
-			a=b;
-			len--;
-		}
-	}
-	len=strlen(x);
-	for(a=0;a<len;a++)
 	{
-		i[a]=x[a]-'0';
-		if(i[a]%2==0)
+		if(x[a]>='0'&&x[a]<='9')
 		{
-			ou++;
-		}
-		else
-		{
-			ji++;
-		}
+			while(x[a+1]>='0'&&x[a+1]<='9')
+			{
+				a++;
+			}
+		    i[a]=x[a]-'0';
+		    if(i[a]%2==0)
+		    {
+			    ou++;
+		    }
+		    else
+	    	{
+			    ji++;
+		    }
+	    }
 	}
 	if(ou>ji)
 	printf("%d",ji);
