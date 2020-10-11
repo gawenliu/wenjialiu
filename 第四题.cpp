10420f7376078e9463c101abbfdd37813a8a6e5a
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-10-11 19:58:47 +0800

    Update 第四题.cpp

--- 第四题.cpp
+++ 第四题.cpp
@@ -2,35 +2,38 @@
 #include<string.h>
 int main() 
 {
-    int num[100];
+    int num=0;
     char zifu[100];
     int x=0;
     int y=0;
+    int z=-1;
     int time=0;
-    int i=-1;
     zifu[0]='1';
     scanf("%s",&zifu);
-    time=strlen(zifu)-1;
-    for(y=0;y<=time;y++)
-    {
-    	if(zifu[y]==',')
-    	{
-    		for(x=y;x<time;x++)
-    		{
-    			zifu[x]=zifu[x+1];
-			}
-			time--;
-		}
-		num[y]=zifu[y]-'0';
-	}
-	for(y=0;y<=time;y++)
+    time=strlen(zifu);
+	for(x=0;x<time;x++)
 	{
-		if(y==num[y])
+		if(zifu[x]==','||zifu[x]=='，')
 		{
-			i=y;
-			break;
+			y++;
 		}
+		else if(zifu[x]>='0'&&zifu[x]<='9')
+		{
+			num=0;
+			while(zifu[x]>='0'&&zifu[x]<='9')
+			{
+				z=zifu[x]-'0';
+				num=num*10+z;
+				x++;
+			}
+			x--;
+			if(y==num)
+			{
+				z=y;
+				break;
+			}
+		} 
 	}
-	printf("%d",i);
+	printf("%d",z);
 	return 0;
 }
